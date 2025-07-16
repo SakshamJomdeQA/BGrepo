7c3da838b6f909571e78a3eba19094b2577a9816
Saksham Jomde <saksham.jomde@contentstack.com>
2025-07-16 08:19:17 +0530

    edge v1

--- functions/[proxy].edge.js
+++ functions/[proxy].edge.js
@@ -1,18 +1,14 @@
 const blueDeploymentHost = "bgrepo.devcontentstackapps.com";
 const greenDeploymentHost = "bgrepo-green.devcontentstackapps.com";
 
-export default async function handler(request) {
-  const randomNumber = Math.floor((Math.random() * 10) + 1);
-  const modifiedUrl = new URL(request.url);
-  console.log(randomNumber);
-  if (randomNumber % 2 === 0) {
-    modifiedUrl.hostname = greenDeploymentHost;
-      console.log(modifiedUrl.hostname);
-  } else {
-    modifiedUrl.hostname = blueDeploymentHost;
-    console.log(modifiedUrl.hostname);
+export default {
+  async fetch(request) {
+    const random = Math.random();
+    const url = new URL(request.url);
+    url.hostname = random > 0.5
+      ? blueDeploymentHost
+      : greenDeploymentHost;
+    console.log("Routing to:", url.hostname);
+    return fetch(new Request(url, request));
   }
-
-  const newRequest = new Request(modifiedUrl, request);
-  return fetch(newRequest);
-}
+};
